f7e604e8c47bf301d1bf5c81e5a8d148dba40ac1
Takahiro Furukawa <takapc.study@gmail.com>
2020-05-28 22:22:51 +0900

    headerテンプレートファイルの追加

---
+++ bungushop/view/templates/header.php
@@ -0,0 +1,14 @@
+<header>
+    <a class="logo" href="<?php print(HOME_URL) ?>">
+        <img src="<?php print(LOGO_PATH .  "logo1.png"); ?>">
+    </a>
+    <div class="welcome">
+        <p>Welcome　to　BUNGU SHOP ！!</p>
+    </div>
+    <div class="nav-item">
+        <a href="<?php print(NEW_ACCOUNT_URL); ?>">サインアップ</a>
+    </div>
+    <div class="nav-item">
+        <a href="<?php print(LOGIN_URL) ?>">ログイン</a>
+    </div>
+</header>
